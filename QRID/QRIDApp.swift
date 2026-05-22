49c79046ca95fc99be5be6a18039a9e10e7afec1
Lucas Li <lucas.li@example.com>
2026-05-22 21:04:18 +0800

    Initial Commit

---
+++ QRID/QRIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRIDApp.swift
+//  QRID
+//
+//  Created by Lucas Li on 2026/5/22.
+//
+
+import SwiftUI
+
+@main
+struct QRIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
